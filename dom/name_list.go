e47839fa1994b5a01a1d807c8f96565a069d666e
deoxxa <deoxxa@fknsrs.biz>
2014-10-12 23:11:57 +1100

    implement NameList methods

--- dom/name_list.go
+++ dom/name_list.go
@@ -1,48 +1,53 @@
 package dom
 
+import (
+	"strings"
+)
+
 type INameList interface {
 	// accessors
-	GetLength() int32
-	SetLength(length int32)
+	GetLength() int
 	// spec-defined functions
-	GetName(index int32) string
-	GetNamespaceURI(index int32) string
+	GetName(index int) string
+	GetNamespaceURI(index int) string
 	Contains(str string) bool
 	ContainsNS(namespaceURI string, name string) bool
 }
 
-type NameList struct {
-	length int32
+type NameList []string
+
+func (n NameList) GetLength() int {
+	return len(n)
 }
 
-func (n *NameList) GetLength() int32 {
-	return n.length
+func (n NameList) GetName(index int) string {
+	str := n[index]
+
+	i := strings.Index(str, "}")
+
+	return str[i:]
 }
 
-func (n *NameList) SetLength(length int32) {
-	n.length = length
+func (n NameList) GetNamespaceURI(index int) string {
+	str := n[index]
+
+	i := strings.Index(str, "}")
+
+	return str[1:i]
 }
 
-func (n *NameList) GetName(index int32) string {
-	panic("unimplemented") // TODO
-
-	return ""
+func (n NameList) Contains(str string) bool {
+	return n.ContainsNS("", str)
 }
 
-func (n *NameList) GetNamespaceURI(index int32) string {
-	panic("unimplemented") // TODO
+func (n NameList) ContainsNS(namespaceURI string, name string) bool {
+	str := "{" + namespaceURI + "}" + name
 
-	return ""
-}
-
-func (n *NameList) Contains(str string) bool {
-	panic("unimplemented") // TODO
-
-	return false
-}
-
-func (n *NameList) ContainsNS(namespaceURI string, name string) bool {
-	panic("unimplemented") // TODO
+	for _, v := range n {
+		if v == str {
+			return true
+		}
+	}
 
 	return false
 }
